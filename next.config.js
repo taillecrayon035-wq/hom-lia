11af6e9ea39097c2f67b84337ebd5e30b9c8eae0
Markovvvvv <taillecrayon035@gmail.com>
2026-04-16 18:14:29 +0200

    fix: move outputFileTracingExcludes out of experimental

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    outputFileTracingExcludes: {
-      '*': ['node_modules/**/*'],
-    },
+  outputFileTracingExcludes: {
+    '*': ['node_modules/**/*'],
   },
 };
 module.exports = nextConfig;
