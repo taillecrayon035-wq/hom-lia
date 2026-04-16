62d8a6b066291a40ca4f571ae87f071262b06502
Markovvvvv <taillecrayon035@gmail.com>
2026-04-16 18:11:30 +0200

    fix: exclude node_modules from build traces

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': ['node_modules/**/*'],
+    },
+  },
+};
 module.exports = nextConfig;
